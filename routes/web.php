849b604dd85098cf0c1d7ea00581d26bee44225f
Teodora <aktr9886@protonmail.com>
2023-12-01 00:52:27 +0200

    commit to save changes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/contact', function () {
+    return view('services');
+})->name('services');
